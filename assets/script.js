c4cd2de35b670844e2162688e33b2e42f9de52aa
Chris Dominy <cdominy@utexas.edu>
2020-08-23 14:24:27 -0500

    Added all files and initialized index

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var apiKey = "13fe265184ae8a7bdc2a5d316e19def2"
+// Current Weather 
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// 5 Day Forecast
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
